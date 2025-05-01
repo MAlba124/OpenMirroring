397e4ed113f7cf3102c0939fec197491ab0f4ea7
Marcus Hanestad <marlhan@proton.me>
2025-05-01 20:03:58 +0200

    android-sender: fix stupid gstreamer

--- android-sender/app/jni/Android.mk
+++ android-sender/app/jni/Android.mk
@@ -3,8 +3,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := android_sender
-LOCAL_SRC_FILES := dummy.cpp
 LOCAL_SHARED_LIBRARIES := gstreamer_android
+LOCAL_SRC_FILES := dummy.cpp
 LOCAL_LDLIBS := -llog -landroid
 include $(BUILD_SHARED_LIBRARY)
 
@@ -28,8 +28,7 @@ endif
 
 GSTREAMER_NDK_BUILD_PATH  := $(GSTREAMER_ROOT)/share/gst-android/ndk-build/
 include $(GSTREAMER_NDK_BUILD_PATH)/plugins.mk
-GSTREAMER_PLUGINS         := $(GSTREAMER_PLUGINS_CORE) $(GSTREAMER_PLUGINS_NET)
-GSTREAMER_EXTRA_DEPS      := gstreamer-app-1.0
-GSTREAMER_EXTRA_LIBS      := -lffi
+GSTREAMER_PLUGINS         := $(GSTREAMER_PLUGINS_CORE) $(GSTREAMER_PLUGINS_NET) $(GSTREAMER_PLUGINS_CODECS) $(GSTREAMER_PLUGINS_CODECS_RESTRICTED)
+GSTREAMER_EXTRA_DEPS      := gstreamer-app-1.0 gstreamer-video-1.0
 
 include $(GSTREAMER_NDK_BUILD_PATH)/gstreamer-1.0.mk
