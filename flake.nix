445b14bc80abb40439d6849482a280635ce2e78a
Marcus Hanestad <marlhan@proton.me>
2025-06-06 18:19:22 +0200

    chore: remove unneeded glibc dependencie

--- flake.nix
+++ flake.nix
@@ -62,7 +62,6 @@
             LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
             BINDGEN_EXTRA_CLANG_ARGS = [
                 ''-I"${pkgs.llvmPackages.libclang.lib}/lib/clang/${pkgs.llvmPackages.libclang.version}/include"''
-                "-I ${pkgs.glibc.dev}/include"
             ];
             LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
 
