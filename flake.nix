fd6527e3b7f7af9634a0484a0983b3e5527adf14
Marcus Hanestad <marlhan@proton.me>
2025-08-31 17:50:52 +0200

    added receiver build to flake.nix

--- flake.nix
+++ flake.nix
@@ -4,9 +4,7 @@
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
   outputs = { self, nixpkgs, flake-utils, rust-overlay }:
@@ -18,14 +16,13 @@
             inherit system overlays;
           };
 
-          rustTargets = [
-            "aarch64-unknown-linux-gnu"
-            "x86_64-unknown-linux-gnu"
-          ];
-
           rustToolchain = pkgs.rust-bin.nightly.latest.complete.override {
-            targets = rustTargets;
+            targets = [
+              "aarch64-unknown-linux-gnu"
+              "x86_64-unknown-linux-gnu"
+            ];
           };
+          releaseRustToolchain = (pkgs.rust-bin.fromRustupToolchain { channel = "1.89.0"; });
         in
         let
           nativeBuildInputs = with pkgs; [
@@ -38,6 +35,7 @@
             slint-lsp
             heaptrack
             slint-viewer
+            wrapGAppsHook3
           ];
           buildInputs = with pkgs; [
             libGL
@@ -70,17 +68,36 @@
             systemdLibs
             libinput
           ];
+
+          newRustPlatform = pkgs.makeRustPlatform {
+            cargo = releaseRustToolchain;
+            rustc = releaseRustToolchain;
+          };
         in
-        with pkgs;
+        let
+          receiver = newRustPlatform.buildRustPackage {
+            pname = "openmirroring";
+            version = "0.1.0";
+            src = ./.;
+            cargoBuildFlags = "-p receiver";
+
+            cargoHash = "sha256-z2B46oM1Q0qDuyGmWScNJnQeaQXZDEMKSpn1zrOATaA=";
+
+            inherit buildInputs nativeBuildInputs;
+          };
+        in rec
         {
-          devShells.default = mkShell {
-            LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+          devShells.default = pkgs.mkShell {
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
             BINDGEN_EXTRA_CLANG_ARGS = [
                 ''-I"${pkgs.llvmPackages.libclang.lib}/lib/clang/${pkgs.llvmPackages.libclang.version}/include"''
             ];
             LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
 
             inherit buildInputs nativeBuildInputs;
+          };
+          packages = {
+            default = receiver;
           };
         }
       );
